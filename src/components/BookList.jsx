8f276116f53217b214423a54b7572909519a92db
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-13 16:52:33 +0100

    added soemthing

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -9,31 +9,38 @@ class BookList extends Component {
 
   render() {
     return (
-      <>
-        <Row>
-          <Col>
-            <Form.Group>
-              <Form.Label>Search a book</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Search here"
-                value={this.state.searchQuery}
-                onChange={(e) => this.setState({ searchQuery: e.target.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          {this.props.books
-            .filter((b) =>
-              b.title.toLowerCase().includes(this.state.searchQuery)
-            )
-            .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
-              </Col>
-            ))}
-        </Row>
+      <><Row>
+        <Col>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Label>Search a book</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Search here"
+                  value={this.state.searchQuery}
+                  onChange={(e) => this.setState({ searchQuery: e.target.value })}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            {this.props.books
+              .filter((b) =>
+                b.title.toLowerCase().includes(this.state.searchQuery)
+              )
+              .map((b) => (
+                <Col xs={12} md={4} key={b.asin}>
+                  <SingleBook book={b} />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col>
+          <h1>here go comments</h1>
+        </Col>
+      </Row>
+
       </>
     )
   }
